e00cbd2d572c36874ad7f7c7d9dcb1df753d5cc5
DevottaAbraham <abdevotta@gmail.com>
2025-11-07 18:49:27 +0530

    Tracked and Added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname), // Set the project root explicitly
   build: {
     outDir: 'dist'
   }
