68237ab6de117f2fb1437aee181b5f9e9523a44e
DevottaAbraham <abdevotta@gmail.com>
2025-11-07 22:59:22 +0530

    Tracked and Added

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-  // root: 'src', // Set 'src' as the project root
-  // build: {
-  //   outDir: '../dist' // Output to a 'dist' folder in the project root
-  // }
-});
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  }
+})
